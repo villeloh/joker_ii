e5f03e707b54ba74501a4f87f41b3faf75c2a3d8
villeloh <villealla@gmail.com>
2019-10-13 08:10:43 +0300

    add scoop routine to manual controls... and a ton of other stuffs to god knows where

--- challenge_5.py
+++ challenge_5.py
@@ -13,16 +13,24 @@ from ev3dev2.sound import Sound
 from ev3dev2.power import PowerSupply
 from ev3dev2.button import Button
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
-from ev3dev2.sensor.lego import ColorSensor, InfraredSensor
+from ev3dev2.sensor.lego import ColorSensor, InfraredSensor , TouchSensor
 from ev3dev2.motor import OUTPUT_A, OUTPUT_D, MoveTank
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM, SpeedPercent
 
 mt = MoveTank(OUTPUT_A, OUTPUT_D)
 irs = InfraredSensor(INPUT_2)
 cs = ColorSensor(INPUT_3) # not used atm
+ts = TouchSensor(INPUT_1)
 
 LEFT = 'LEFT'
 RIGHT = 'RIGHT'
+
+def moveback() :
+    mt.on_for_seconds(-45,-45 , 0.5)
+
+def touched() :
+    if ts.is_pressed :
+        moveback()
 
 def main():
 
@@ -31,28 +39,128 @@ def main():
     play_beep()
 
     irs.on_channel1_top_left = top_left_channel_1_action
+    irs.on_channel1_top_right = top_right_channel_1_action
+    irs.on_channel1_bottom_left = bottom_left_channel_1_action
+    irs.on_channel1_bottom_right = bottom_right_channel_1_action
 
     while True:
         irs.process()
         time.sleep(0.01)
 
-
-def sweeping_turn(direction):
+def sweeping_turn(direction, seconds):
     if direction == LEFT:
-        mt.on_for_seconds(50, 80, 2, block=False)
+        mt.on_for_seconds(40, 70, seconds)
     elif direction == RIGHT:
-        mt.on_for_seconds(80, 50, 2, block=False)
+        mt.on_for_seconds(70, 40, seconds)
+
+#def check_hole()
+
+def Off_WhenWalls(dir) :
+     if not irs.proximity  < 15 * 1.4  :
+
+        while not irs.proximity  < 15 * 1.4 :
+                straight_ahead(90,0.8)
+                if irs.proximity  < 15 * 1.4 :
+                    #play_beep(length=3)
+                    moveback()
+                    time.sleep(2)
+                sweeping_turn(dir, 1)
+                dir = TriggerWhite()
+
+        #play_beep(length=3)
+        time.sleep(0.01)
+        while irs.proximity  < 15 * 1.4 :
+            if not irs.proximity  < 15 * 1.4 :
+                touched()
+                straight_ahead(90,0.8)
+
 
 def top_left_channel_1_action(state):
+
     if state:
-        straight_ahead(90, 2)
-        sweeping_turn(LEFT)
-        straight_ahead(90, 2)
-        sweeping_turn(RIGHT)
-        # continue to the other gate
+        # straight_ahead(90, 3)
+        dir = TriggerWhite()
+        sweeping_turn(LEFT, 3)
+
+
+    if not irs.proximity  < 15 * 1.4  :
+
+        while not irs.proximity  < 15 * 1.4 :
+                touched()
+                straight_ahead(90,0.8)
+                if irs.proximity  < 15 * 1.4 :
+                    #play_beep(length=3)
+                    moveback()
+                    time.sleep(2)
+                sweeping_turn(dir, 1)
+                dir = TriggerWhite()
+                #sweeping_turn(dir, 3)
+        #play_beep(length=3)
+        time.sleep(0.01)
+        while irs.proximity  < 15 * 1.4 :
+            if not irs.proximity  < 15 * 1.4 :
+                touched()
+                straight_ahead(90,0.8)
+    Off_WhenWalls(dir)
+
+def top_right_channel_1_action(state):
+    if state:
+        # straight_ahead(90, 3)
+        sweeping_turn(LEFT, 4)
+
+        firstMove = False
+        secondMove = False
+
+        while not firstMove:
+            if irs.proximity * 0.7 > 60:
+                straight_ahead(90, 3)
+                #play_beep(length=1)
+                firstMove = True
+            time.sleep(0.01)
+        while not secondMove:
+            if irs.proximity * 0.7 > 60:
+                straight_ahead(90, 3)
+                #play_beep(length=3)
+                secondMove = True
+            time.sleep(0.01)
+
+def bottom_right_channel_1_action(state):
+    if state:
+        # straight_ahead(90, 3)
+        sweeping_turn(LEFT, 5)
+
+        firstMove = False
+        secondMove = False
+
+        while not firstMove:
+            if irs.proximity * 0.7 > 60:
+                straight_ahead(90, 3)
+                firstMove = True
+                #play_beep(length=3)
+            time.sleep(0.01)
+        while not secondMove:
+            if irs.proximity * 0.7 > 60:
+                straight_ahead(90, 3)
+                #play_beep(length=3)
+                secondMove = True
+            time.sleep(0.01)
+
+def bottom_left_channel_1_action(state):
+        if state:
+            straight_ahead(90, 3)
+            time.sleep(7.5)
+            reverse(90, 3)
+            time.sleep(12)
+            straight_ahead(90, 3)
+            # continue to the other gate
+
+
 
 def straight_ahead(speed, seconds):
     mt.on_for_seconds(speed, speed, seconds)
+
+def reverse(speed, seconds):
+    mt.on_for_seconds(-speed, -speed, seconds)
 
 def turn(direction, degrees):
 
@@ -97,5 +205,20 @@ LOW_BATTERY_VOLTAGE_THRESHOLD = 5.0
 MIDDLE_VOLTAGE_THRESHOLD = 7.0
 
 # not sure if this is needed; let's keep it for now
+
+def TriggerWhite() :
+    direction = 'LEFT'
+    changeCount = 0
+    if cs.color_name == 'White':
+        if changeCount == 0 :
+            changeCount = changeCount + 1
+            direction = 'LEFT'
+            play_beep(length=2.0)
+        if changeCount == 1 :
+            changeCount = changeCount - 1
+            direction = 'RIGHT'
+            play_beep(length=2.0)
+    return direction
+
 if __name__ == '__main__':
     main()
